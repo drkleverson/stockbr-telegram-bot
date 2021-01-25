4384bb8ebe1c5765def090afa0751443da432a58
Kleverson Bona <kleverson@evoplan.com.br>
2021-01-25 15:02:07 -0300

    update

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,5 +1,6 @@
 async function webhook(req, res) {
   console.log(req.query);
+  console.log(req.query);
 
   const stockResponse = await fetch(
     `https://bovespa.nihey.org/api/quote/${req.query.stock}/2021-01-22`
