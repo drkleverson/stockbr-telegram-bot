556cc31e9fb4672e225bcad185b18e32960371dd
Kleverson Bona <kleverson@evoplan.com.br>
2021-01-25 16:16:16 -0300

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,13 +1,26 @@
 async function webhook(req, res) {
-  console.log(req.query);
-  console.log(req.body);
+  let message = req.body.message;
+
+  if (!message.text.indexOf("@stockbrbot")) {
+    return false;
+  }
+
+  let pieces = message.text.split(" ");
 
   const stockResponse = await fetch(
-    `https://bovespa.nihey.org/api/quote/${req.query.stock}/2021-01-22`
+    `https://bovespa.nihey.org/api/quote/${pieces[1]}/2021-01-22`
   );
+
   const stockResponseJson = await stockResponse.json();
-  console.log(stockResponseJson);
-  res.json([req.query, req.body]);
+
+  await axios.post(
+    "https://api.telegram.org/bot1555054396:AAGOhY8_3KbwjVPZgoBtKII1XTn5WyggB9Q/sendMessage",
+    {
+      chat_id: message.chat.id,
+      reply_to_message_id: message.message_id,
+      text: stockResponseJson.nomres,
+    }
+  );
 }
 
 export default webhook;
