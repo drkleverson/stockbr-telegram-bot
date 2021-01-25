6a074bdaa894c4cfb2b5f0c7143943e45dad9917
Kleverson Bona <dr@kleverson.com>
2021-01-25 18:32:43 -0300

    update

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,19 +2,19 @@ const axios = require("axios").default;
 
 async function webhook(req, res) {
   console.log(req.body.message);
-  /*
+
   if (!message.text.indexOf("@stockbrbot")) {
     return false;
   }
 
   let pieces = message.text.split(" ");
-  
+
   const stockResponse = await fetch(
     `https://bovespa.nihey.org/api/quote/${pieces[1]}/2021-01-22`
   );
 
   const stockResponseJson = await stockResponse.json();
-    
+
   await axios.post(
     "https://api.telegram.org/bot1555054396:AAGOhY8_3KbwjVPZgoBtKII1XTn5WyggB9Q/sendMessage",
     {
@@ -22,7 +22,7 @@ async function webhook(req, res) {
       reply_to_message_id: message.message_id,
       text: stockResponseJson.nomres,
     }
-  );*/
+  );
   res.json([]);
 }
 
