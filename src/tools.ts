bb8d86d0822b60dfe0c9ab3e460fc242fe0a39c8
Luiz Machado <luizfilipe2557@gmail.com>
2021-01-27 01:32:10 -0300

    simplificado arquivo

--- src/tools.ts
+++ src/tools.ts
@@ -1,17 +1,13 @@
-export function moneyFormat(value: string) {
+export function moneyFormat(value: string): string {
   return parseFloat(value)
     .toFixed(2)
     .replace(".", ",")
     .replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
 }
 
-export function timePtBr(time = Date()) {
-  
-  const data = new Date(
-    new Date(time).toLocaleString("pt-br", {
-      timeZone: "America/Sao_Paulo",
-    })
-  );
+export function timePtBr(time = Date()): string {
+
+  const data = new Date(datePtBr(time));
 
   const hora = data.getHours().toString();
   const horaF = hora.length === 1 ? "0" + hora : hora;
@@ -21,13 +17,13 @@ export function timePtBr(time = Date()) {
   return horaF + ":" + minutoF;
 }
 
-export function datePtBr(date = Date()) {
+export function datePtBr(date = Date()): string {
   return new Date(date).toLocaleString("pt-br", {
     timeZone: "America/Sao_Paulo",
   });
 }
 
-export function getPercentageChange(oldNumber: number, newNumber: number) {
+export function getPercentageChange(oldNumber: number, newNumber: number): string {
   const decreaseValue = newNumber - oldNumber;
   const result = (decreaseValue / oldNumber) * 100;
   const mathSymbol = result > 0 ? "+" : "";
